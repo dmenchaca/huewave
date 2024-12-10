857e5526895e346136101b4160957ea0b330b3d1
diegomenchaca <38196300-diegomenchaca@users.noreply.replit.com>
2024-12-10 07:24:02 +0000

    Agent query: Has the TypeScript error been resolved in the alert-dialog.tsx file?

--- client/src/components/ui/alert-dialog.tsx
+++ client/src/components/ui/alert-dialog.tsx
@@ -1,5 +1,6 @@
 import * as React from "react"
 import * as AlertDialogPrimitive from "@radix-ui/react-alert-dialog"
+import { type VariantProps } from "class-variance-authority"
 
 import { cn } from "@/lib/utils"
 import { buttonVariants } from "@/components/ui/button"
